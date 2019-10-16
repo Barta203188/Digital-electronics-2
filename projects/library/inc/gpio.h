83c22407c2041c7aa8273fee132ee016e86129e4
lab661 <lab661@vutbr.cz>
2019-10-16 11:42:29 +0200

    04-interrupts

--- projects/library/inc/gpio.h
+++ projects/library/inc/gpio.h
@@ -3,8 +3,8 @@
 
 #include <avr/io.h>
 
-/*#define PIN_LOW 0
-#define PIN_HIGH 1*/
+#define PIN_LOW 0
+#define PIN_HIGH 1
 
 void GPIO_config_output(volatile uint8_t *reg_name, uint8_t pin_num);
 void GPIO_config_input_nopull(volatile uint8_t *ddr_name, volatile uint8_t *port_name, uint8_t pin_num);
