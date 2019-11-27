b31fe01f89d6d45a989fa281dce6e5c3d4238714
lab661 <lab661@vutbr.cz>
2019-11-27 11:31:22 +0100

    Segment repair

--- projects/10-Project/main.c
+++ projects/10-Project/main.c
@@ -1,51 +1,90 @@
+/*
+ * ---------------------------------------------------------------------
+ * Author:      Tomas Fryza
+ *              Dept. of Radio Electronics, Brno Univ. of Technology
+ * Created:     2018-10-11
+ * Last update: 2019-10-16
+ * Platform:    ATmega328P, AVR 8-bit Toolchain 3.6.2
+ * ---------------------------------------------------------------------
+ * Description: Decimal counter with data display on 7-segment display.
+ * TODO: Create and test functions to operate a 7-segment display using
+ *       shift registers.
+ * NOTE: Store segment.c and segment.h files in library/src and 
+ *       library/inc folders.
+ */
+
+/* Includes ----------------------------------------------------------*/
 #include <avr/io.h>
-#include <TM1637Display.h>
+#include "gpio.h"
+#include "timer.h"
+#include <avr/interrupt.h>
 
-#define CLK 2
-#define DIO 3
+/* Typedef -----------------------------------------------------------*/
+/* Define ------------------------------------------------------------*/
+#define CLK             PD2
+#define DATA            PD4 
 
-const uint8_t AHOJ[] = { 
-SEG_A | SEG_B | SEG_C | SEG_E | SEG_F | SEG_G, // A 
-SEG_B | SEG_C | SEG_E | SEG_F | SEG_G, // H 
-SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F, // O 
-SEG_B | SEG_C | SEG_D | SEG_E // J 
-}; 
-uint8_t desetinna_tecka[] = { 0x00, 0x00, 0x00, 0x00, 0x00 }; 
-//instance displej z knihovny TM1637 
-TM1637Display displej(CLK, DIO);
-void setup() { 
-Serial.begin(9600); 
-displej.setBrightness(15); 
-displej.setSegments(AHOJ); 
-delay(3000); 
-displej.showNumberDec(12, false);
-delay(1000); 
-displej.showNumberDec(34, true); 
-delay(1000); 
-for (int i = 0; i < 256; i++) { 
-Serial.print("Aktualni cislo: "); Serial.println(i); 
-displej.setSegments(desetinna_tecka); 
-desetinna_tecka[0]++; 
-desetinna_tecka[1]++; 
-desetinna_tecka[2]++; 
-desetinna_tecka[3]++; 
-desetinna_tecka[4]++; 
-delay(500); 
-} 
-displej.showNumberDec(5, 1, 2); 
-delay(1000);
-} 
-void loop() 
-{ 
-uint8_t vypis[] = { 0, 0, 0, 0 }; 
-long cas = millis() / 1000; 
-vypis[0] = displej.encodeDigit((cas / 60) / 10); 
-vypis[1] = displej.encodeDigit((cas / 60) % 10); 
-vypis[2] = displej.encodeDigit((cas % 60) / 10); 
-vypis[3] = displej.encodeDigit((cas % 60) % 10); 
-displej.setSegments(vypis); 
-delay(500); 
-vypis[1] = vypis[1] + 128; 
-displej.setSegments(vypis);
-delay(500); 
+/* Variables ---------------------------------------------------------*/
+/* Function prototypes -----------------------------------------------*/
+
+/* Functions ---------------------------------------------------------*/
+/**
+ *  Brief:  Main program. Shows decimal values ​​on 7-segment display.
+ *  Input:  None
+ *  Return: None
+ */
+    int i = 0;
+int main(void)
+{
+    /* D1 led */
+    // TODO: Configure D1 led at Multi-Function Shield
+    //GPIO_config_output(&DDRB , LED_D1);
+    /* Pin Change Interrupts 11:9 */
+    // TODO: Configure Pin Change Interrupts 11, 10, and 9
+    PCICR |= _BV(PCIE1);
+    PCMSK1 |= _BV(PCINT9) | _BV(PCINT10) | _BV(PCINT11);
+
+    TIM_config_prescaler(TIM1, TIM_PRESC_256);
+    TIM_config_interrupt(TIM1, TIM_OVERFLOW_ENABLE);
+    /* 7-segment display interface */
+    // TODO: Configure 7-segment display pins
+    
+    /* Enable interrupts by setting the global interrupt mask */
+    sei();
+
+    /* Infinite loop */
+    for (;;) {
+        // TODO: Use function to display digit 1 at position 0
+        SEG_putc(i, 0);
+    }
+
+    return (0);
 }
+
+/**
+ *  Brief: Pin Change Interrupt 11:9 routine. Toggle a LED.
+ */
+ISR(TIMER1_OVF_vect)
+{
+    // TODO: Toggle a led
+     i++;
+     SEG_putc(i, 0);
+    if(i == 10) 
+        {
+            i = 0;
+            
+        }
+}
+/*ISR(TIMER0_OVF_vect)
+{
+    
+    c++;
+    if(c == 4)
+    {
+        c = 0;
+    }
+    
+}*/
+
+
+
