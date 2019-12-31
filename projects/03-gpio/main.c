19c1074c6a20d3be4a583024565fb0ce475272b3
Barta203188 <49916507+Barta203188@users.noreply.github.com>
2019-12-31 12:32:18 +0100

    modification for exam

--- projects/03-gpio/main.c
+++ projects/03-gpio/main.c
@@ -12,28 +12,15 @@
  * Reference Manual).
  */
 
-/* Includes ----------------------------------------------------------*/
+
 #include <avr/io.h>
 #include <util/delay.h>
 #include "gpio.h"
-
-/* Typedef -----------------------------------------------------------*/
-/* Define ------------------------------------------------------------*/
 #define LED_RED     PD2
 #define LED_GREEN   PB5
 #define BTN         PD0
 #define BLINK_DELAY 500
 
-
-/* Variables ---------------------------------------------------------*/
-/* Function prototypes -----------------------------------------------*/
-
-/* Functions ---------------------------------------------------------*/
-/**
-  * Brief:  Main program. Blink a LED with delay function.
-  * Input:  None
-  * Return: None
-  */
 int main(void)
 {
     /* Set output pin */
@@ -41,24 +28,16 @@ int main(void)
     GPIO_config_output(&DDRD, LED_RED);
     GPIO_write(&PORTD, LED_RED, 1);
     GPIO_write(&PORTB, LED_GREEN, 1);
-     
     
-    //PORTB &= ~_BV(LED_GREEN);         /* PORTB = PORTB and (0010 0000) */
-    //PORTB &= ~_BV(LED_RED);
-    /* Infinite loop */
     for (;;)
     {
         if(bit_is_clear(PIND, BTN))
         {
         /* Invert LED and delay */
-        
         GPIO_toggle(&PORTD, LED_RED);
         GPIO_toggle(&PORTB, LED_GREEN);
         _delay_ms(BLINK_DELAY);     /* Wait for several milisecs */
         }
-       
-        
     }
-
     return (0);
 }
