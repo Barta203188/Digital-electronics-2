88e4f891065d5aa75820c61d4439e9d200c054e5
Barta203188 <49916507+Barta203188@users.noreply.github.com>
2020-01-01 14:54:20 +0100

    modification for exam

--- projects/08-twi_scan/main.c
+++ projects/08-twi_scan/main.c
@@ -36,39 +36,25 @@ state_t current_state = IDLE_STATE;
 void fsm_twi_scanner(void);
 
 /* Functions ---------------------------------------------------------*/
-/**
- *  Brief:  Main program. Test all TWI slave addresses and scan 
- *          connected devices.
- *  Input:  None
- *  Return: None
- */
 int main(void)
 {
     // UART: asynchronous, 8-bit data, no parity, 1-bit stop
     uart_init(UART_BAUD_SELECT(UART_BAUD_RATE, F_CPU));
-
     // TWI
     twi_init();
-
-    /* Timer1
-     * TODO: Configure Timer1 clock source and enable overflow 
-     *       interrupt */
-     
+    // Timer1
     TIM_config_prescaler(TIM1 ,TIM_PRESC_8);
     TIM_config_interrupt(TIM1 ,TIM_OVERFLOW_ENABLE);
-
     // Enables interrupts by setting the global interrupt mask
     sei();
-
     // Put strings to ringbuffer for transmitting via UART.
     uart_puts("\r\n---TWI scanner---");
-//    uart_puts("\r\n     0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f");
+    // uart_puts("\r\n     0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f");
 
     // Infinite loop
     for (;;) {
     }
 
-    // Will never reach this
     return (0);
 }
 
