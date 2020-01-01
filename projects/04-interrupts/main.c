14f6302384f66f07436e9f6fbb122885e6425f66
Barta203188 <49916507+Barta203188@users.noreply.github.com>
2020-01-01 12:20:26 +0100

    Modification for exam

--- projects/04-interrupts/main.c
+++ projects/04-interrupts/main.c
@@ -14,58 +14,35 @@
  *       folders.
  */
 
-/* Includes ----------------------------------------------------------*/
+
 #include <avr/io.h>
 #include "gpio.h"
 #include "timer.h"
 #include <avr/interrupt.h>
-
-/* Typedef -----------------------------------------------------------*/
-/* Define ------------------------------------------------------------*/
 #define LED_GREEN   PB5
 #define LED_RED     PB0
 
-/* Variables ---------------------------------------------------------*/
-/* Function prototypes -----------------------------------------------*/
-
-/* Functions ---------------------------------------------------------*/
 int i = 0;
 int main(void)
 {
-    /* LED_GREEN */
     GPIO_config_output(&DDRB, LED_GREEN);
     GPIO_write(&PORTB, LED_GREEN, PIN_LOW);
-    /* LED_RED */
-    // TODO: Configure red LED
     GPIO_config_output(&DDRB, LED_RED);
     GPIO_write(&PORTB, LED_RED, PIN_LOW);
-
-    /* Set Timer0 */
     TIM_config_prescaler(TIM0, TIM_PRESC_1024);
     TIM_config_interrupt(TIM0, TIM_OVERFLOW_ENABLE);
-
-    /* Set Timer1 */
-    // TODO: Configure Timer1 clock source and enable overflow interrupt
     TIM_config_prescaler(TIM1, TIM_PRESC_256);
     TIM_config_interrupt(TIM1, TIM_OVERFLOW_ENABLE);
-
-
-    /* Enable interrupts by setting the global interrupt mask */
     sei();
 
-    /* Infinite loop */
     for (;;) {
     }
 
     return (0);
 }
 
-/**
- *  Brief: Timer/Counter0 overflow interrupt routine. Toggle green LED.
- */
 ISR(TIMER0_OVF_vect)
 {
-    
     i++;
     if (i==10)
     {
@@ -75,9 +52,6 @@ ISR(TIMER0_OVF_vect)
     }
 }
 
-/**
- *  Brief: Timer/Counter1 overflow interrupt routine. Toggle red LED.
- */
 ISR(TIMER1_OVF_vect)
 {
     // TODO: Toggle red LED
